142f67467c693b2744fc41b05581ce7ab9fb3f0e
Ahrorxo'ja A'zamxo'jayev <azamxudjayev@MacBook-Pro-Ahrorxoja.local>
2020-10-06 19:48:07 +0500

    Updates

---
+++ precache-manifest.acc5d8eda6b95da46af2e57ff838f28e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f918a4d787cb0fa3ee680d4c77dcd1b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ee5ebd575451c41aa06",
+    "url": "/monsters-rolodex/static/css/main.d1bb1ae9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee5ebd575451c41aa06",
+    "url": "/monsters-rolodex/static/js/main.ec9edd72.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
